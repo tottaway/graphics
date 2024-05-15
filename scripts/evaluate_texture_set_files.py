6b5006af12f2c23c11bbc45d14f82a3ddcc94720
tottaway <tottaway123@gmail.com>
2024-05-14 22:28:46 -0400

    vizualization script

--- scripts/evaluate_texture_set_files.py
+++ scripts/evaluate_texture_set_files.py
@@ -1,21 +1,42 @@
+from typing import Optional
 import yaml
 import argparse
 from matplotlib import pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
+import numpy as np
 
+def visualize_subsection(subsection_node, full_image):
+    start = np.array(subsection_node["start"])
+    tile_size = np.array(subsection_node["tile_size"])
+    for i in range(1, subsection_node["horizontal_tile_count"]):
+        for j in range(1, subsection_node["vertical_tile_count"]):
+            # __import__('pdb').set_trace()
+            tile_start = start + tile_size * np.array([i, j])
+            tile_end = tile_start + tile_size
+            img_view = full_image[tile_start[1]:tile_end[1], tile_start[0]:tile_end[0]] 
+            plt.imshow(img_view)
+            plt.show()
 
-def vizualize_tile_set(path: str):
+def visualize_file(file_node, subsection_name: str):
+    img = np.asarray(Image.open(file_node["file_name"]))
+    for curr_subsection_name, subsection in file_node["subsections"].items():
+        if not subsection_name or subsection_name == curr_subsection_name:
+            visualize_subsection(subsection, img)
+
+def visualize_tile_set(path: str, subsection_name: str):
     with open(path) as stream:
         tile_set_description = yaml.safe_load(stream)
     for file in tile_set_description:
-        img = mpimg.imread(file["file_name"])
-        plt.imshow(img)
-        plt.show()
+        visualize_file(file, subsection_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('texture_set_path', type=str,
                         help='path to a yaml file describing a texture set')
+
+    parser.add_argument('-s', '--subsection-name', type=str,
+                        help='name of the subsection to display', default="", required=False)
+
     args = parser.parse_args()
-    vizualize_tile_set(args.texture_set_path)
+    visualize_tile_set(args.texture_set_path, args.subsection_name)
